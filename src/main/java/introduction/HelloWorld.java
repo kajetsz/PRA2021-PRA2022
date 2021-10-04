a10bdd5fcf9030b07a1bc6fc0cc2ad98c564d620
kajetsz <kajsza@st.amu.edu.pl>
2021-10-04 18:17:56 +0200

    add class 1 solution

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+
+    final static Logger log = Logger.getLogger(HelloWorld.class);
 
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+        log.info("dzien dobry");
     }
 }
